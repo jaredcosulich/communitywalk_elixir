94c1e0cb5dc559fa0f8e43f1cd8020502af32eb0
Jared Cosulich <jared.cosulich@gmail.com>
2021-03-06 10:09:50 -0500

    trying build release

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :fowe_template, FoweTemplateWeb.Endpoint,
   url: [host: "example.com", port: 80],
+  pool_size: 2,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
